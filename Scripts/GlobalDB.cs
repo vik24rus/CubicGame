9e2dedf6978957dc8b2baa1533050f726590dbf0
vik24rus <vik24rus@gmail.com>
2017-08-22 18:29:25 +0700

    добавлена задержка получения урона

--- Scripts/GlobalDB.cs
+++ Scripts/GlobalDB.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class GlobalDB : MonoBehaviour {
     public int playerHP = 100;
     public int sphereHP = 100;
-    public int attakSphere = 25;
+    public int attakSphere = 15;
     public int attakPlayer = 5;
     public string PlayerName = "RigidBodyFPSController";
 
     bool PlayerDIE = false;
+
+    
     // Use this for initialization
     void Start () {
 		
@@ -17,52 +19,14 @@ public class GlobalDB : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
-
-    //public void PlayerGetDamage()
-    //{
-
-    //    while (playerHP > 0)
-    //    {
-    //        playerHP = playerHP - attakSphere;
-
-    //        if (playerHP <= 0)
-    //        {
-    //            Debug.Log("УБИТ");
-    //        }
-
-    //    }
-
-    //}ЗАДЕРЖКА НУЖНА!
-    public void Go()
-    {
-        StartCoroutine(PlayerGetDamage());
+        if (playerHP <= 0)
+        {
+            //Debug.Log("УБИТ");
+        }
     }
 
-    public void Go2()
-    {
-        StopCoroutine(PlayerGetDamage());
-    }
-    public IEnumerator PlayerGetDamage()
-    {
-
-        
-            playerHP = playerHP - attakSphere;
-
-            if (playerHP <= 0)
-            {
-                Debug.Log("УБИТ");
-            }
-            yield return new WaitForSeconds(1000);
-
-        
-
-    }
-
-
-
-
+           
+    
     public void SphereGetDamage()
     {
 
--- Scripts/Sphere.cs
+++ Scripts/Sphere.cs
@@ -6,44 +6,47 @@ using UnityEngine;
 public class Sphere : MonoBehaviour {
 
     private GlobalDB _GDB;
+
+    public float AttackDelay;
+    private float _attackDelay;
     
-    // Use this for initialization
     void Start () {
         _GDB = GameObject.Find("GameManager").GetComponent<GlobalDB>(); //поиск скрипта по всем объектам в игре
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+       
+    }
 
 
-    //void OnTriggerEnter(Collider other)
-    //{
-    //    if (other.name.ToString() == _GDB.PlayerName)
-    //    {
-    //        //_GDB.PlayerGetDamage();
-    //        _GDB.Go();
-    //    }
-
-    //}
-
-    //void OnTriggerExit(Collider other)
-    //{
-    //    if (other.name.ToString() == _GDB.PlayerName)
-    //    {
-    //        //_GDB.PlayerGetDamage();
-    //        _GDB.Go2();
-    //    }
-
-    //}
-    void OnTriggerStay(Collider other)
-    {
-        if (other.name.ToString() == _GDB.PlayerName)
+   void OnTriggerStay(Collider other) 
+   {
+        if (_attackDelay < AttackDelay)//если _задержка меньше Задержки которую указали вы то - ждем пока не накапают секунды до того числа которое мы указали и если накапали то идем дальше
         {
-            //_GDB.PlayerGetDamage();
-            _GDB.Go();
-        }
+            _attackDelay += Time.deltaTime;//прибавляем 1 сек
+            
 
+        } else
+        {
+
+            if (other.name.ToString() == _GDB.PlayerName)
+            {
+
+                _GDB.playerHP = _GDB.playerHP - _GDB.attakSphere;
+                _attackDelay = 0f;//обнуляем _зажержку
+
+            }
+
+        }
+        
+    }
+
+    void OnTriggerExit()
+    {
+        _attackDelay = 0;//обнуление переменной при выходе из триггера
+
+
+        //все работает урон наноситься раз в секунду. ой раз в определенное вами время. не забывайте указать тег у игрока. всем спасибо досвидание.
     }
 }
